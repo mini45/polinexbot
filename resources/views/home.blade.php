91b21378f7c296fa4a5cfff92e6d1aee589bb20a
mini45 <andre.muenstermann@gmail.com>
2017-06-28 23:20:32 +0200

    init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+
+@section('title', 'Page Title')
+
+@section('content')
+    sadad
+@endsection
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>App Name - @yield('title')</title>
+    @section('styles')
+</head>
+<body>
+
+<div class="container">
+    @yield('content')
+</div>
+</body>
+@section('scripts')
+
+</html>
